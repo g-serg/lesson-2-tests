3fec98225300439d799e44ba3ade2eddad46b58a
Sergey Gorelov <s.gorelov@norsi-trans.ru>
2017-11-11 23:30:21 +0300

    app test done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import NewsPost from './NewsPost';
+
+let id = 0;
+
+function getId() {
+  return ++id;
+}
 
 class App extends Component {
+  state = {
+    newsInput: '',
+    news: []
+  };
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({ newsInput: value });
+  };
+
+  handleKeyDown = event => {
+    if (event.keyCode === 13) {
+      const { newsInput, news } = this.state;
+      const NewsPost = { text: newsInput, id: getId() };
+      this.setState({ newsInput: '', news: [...news, NewsPost] });
+    }
+  };
+
   render() {
-    return <div />;
+    const { newsInput, news } = this.state;
+    return (
+      <div>
+        <input
+          value={newsInput}
+          type="text"
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
+        />
+        <div className="App">{news.map(() => <NewsPost />)}</div>
+      </div>
+    );
   }
 }
 
